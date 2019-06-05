c6a939a1958f0e477ae627c19db76a4846813caa
zhangrongxin <zhangrongxin@ibeesaas.com>
2019-06-05 16:37:43 +0800

    init

---
+++ src/main/java/com/ibeesaas/demo/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.ibeesaas.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello world";
+    }
+}
