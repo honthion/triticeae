dafdf1df5d450f86f94cf26c8e0eeabeb813f3bf
zhangrongxin <zhangrongxin@ibeesaas.com>
2019-06-05 18:15:13 +0800

    junit is ok

--- src/main/java/com/ibeesaas/demo/web/HelloController.java
+++ src/main/java/com/ibeesaas/demo/web/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String index() {
-        return "Hello world";
+        return "Hello world 1";
     }
 }
--- src/test/java/com/ibeesaas/demo/DemoApplicationTests.java
+++ src/test/java/com/ibeesaas/demo/DemoApplicationTests.java
@@ -1,16 +1,49 @@
 package com.ibeesaas.demo;
 
+import com.ibeesaas.demo.web.HelloController;
+import org.hamcrest.core.IsEqual;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//对JUnit4的支持
+@RunWith(SpringJUnit4ClassRunner.class)
+//开启web应用的配置，用于模拟servletContext
+@WebAppConfiguration
+// 指定启动类 替代SpringApplicationConfig
+@SpringJUnitConfig(classes = DemoApplication.class)
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    public MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void test1() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(IsEqual.equalTo("hello world")));
+    }
 
 }
