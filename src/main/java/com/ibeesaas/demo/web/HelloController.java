b843505f56330d110f123f3086f03bc0dcc3ae39
zhangrongxin <zhangrongxin@ibeesaas.com>
2019-06-05 19:12:42 +0800

    add actuator

--- src/main/java/com/ibeesaas/demo/web/HelloController.java
+++ src/main/java/com/ibeesaas/demo/web/HelloController.java
@@ -1,13 +1,25 @@
 package com.ibeesaas.demo.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class HelloController {
+    @Value("${environments.dev.name}")
+    private String env;
+    @Value("${author.desc}")
+    private String desc;
+    @Value("${author.age}")
+    private Integer age;
 
     @RequestMapping("/hello")
-    public String index() {
-        return "Hello world 1";
+    public Object index() {
+
+        return new HashMap<String, String>() {{
+            put(env, desc + "  " + age);
+        }};
     }
 }
